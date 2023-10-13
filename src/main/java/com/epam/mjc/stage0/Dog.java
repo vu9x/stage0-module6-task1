d8a11630a14b936f903ce2c30fc48682a3896570
Vu <nguyentuanvu9x@gmail.com>
2023-10-13 16:16:53 +0300

    Updating: Dog. Deleting overrided method: getDescription()

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -4,10 +4,4 @@ public class Dog extends Animal{
     public Dog (){
         super("brown", 4, true);
     }
-
-    @Override
-    public String getDescription(){
-        String result = super.getDescription() + " Moreover, it barks all the time.";
-        return result;
-    }
 }
