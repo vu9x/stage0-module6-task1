7387ba382c906612585f6f2135b8e77d3769bded
Vu <nguyentuanvu9x@gmail.com>
2023-10-13 16:04:08 +0300

    Done: Bird, Dog

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,13 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal{
+    public Dog (){
+        super("brown", 4, true);
+    }
 
+    @Override
+    public String getDescription(){
+        String result = super.getDescription() + "  Moreover, it barks all the time.";
+        return result;
+    }
 }
